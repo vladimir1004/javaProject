1dae4989592203dcb44bced61a202654c38e95eb
Karakis Vladimir <0975059295v@gmail.com>
2017-05-13 19:59:33 +0300

    new class
    yes

---
+++ src/test/java/combaranzev/SampleTestNgTest.java
@@ -0,0 +1,25 @@
+package combaranzev;
+
+import org.openqa.selenium.support.PageFactory;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import combaranzev.pages.HomePage;
+
+public class SampleTestNgTest extends TestNgTestBase {
+
+  private HomePage homepage;
+
+  @BeforeMethod
+  public void initPageObjects() {
+    homepage = PageFactory.initElements(driver, HomePage.class);
+  }
+
+  @Test
+  public void testHomePageHasAHeader() {
+    driver.get(baseUrl);
+    Assert.assertFalse("".equals(homepage.header.getText()));
+  }
+}
