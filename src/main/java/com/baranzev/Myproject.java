8eba3d69bff84cd38704b76403e6c2758f204e68
Karakis Vladimir <0975059295v@gmail.com>
2017-05-14 23:55:56 +0300

    change method
    change method

--- src/main/java/com/baranzev/Myproject.java
+++ src/main/java/com/baranzev/Myproject.java
@@ -2,25 +2,22 @@ package com.baranzev;
 
 public class Myproject {
 
-  public static void main(String[] args){
-   hello("user");
- Square s = new Square(5);
+  public static void main(String[] args) {
+    hello("user");
+    Square s = new Square(5);
 
-       System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-       Rectangle r = new Rectangle(4, 6);
+    Rectangle r = new Rectangle(4, 6);
 
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " and " + r.b +  " = " + area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " and " + r.b + " = " + r.area());
 
   }
-  public static void hello(String somebody){
+
+  public static void hello(String somebody) {
 
     System.out.println("Hello " + somebody + "!");
   }
-  public static double area (Square s){
-    return  s.l*s.l;
-  }
-  public static double area (Rectangle r ){
-    return r.a * r.b ;
-  }
+
+
 }
--- src/main/java/com/baranzev/Rectangle.java
+++ src/main/java/com/baranzev/Rectangle.java
@@ -9,4 +9,7 @@ public class Rectangle {
     this.a = a;
     this.b = b;
   }
+  public  double area ( ){
+    return this.a * this.b ;
+  }
 }
