e58839961a21a38d75bba2a998123e85d5c34ec6
Karakis Vladimir <0975059295v@gmail.com>
2017-05-21 01:31:45 +0300

    do if

--- src/main/java/com/baranzev/Equaition.java
+++ src/main/java/com/baranzev/Equaition.java
@@ -7,29 +7,32 @@ public class Equaition {
   private double c;
 
   private int n;
-// doing construction
-  public Equaition(double a, double b, double c){
+
+  // doing construction
+  public Equaition(double a, double b, double c) {
 
     this.a = a;
     this.b = b;
     this.c = c;
 
-    double d = b*b - 4*a*c;
-    if(d>0){
-      n=2;
+    double d = b * b - 4 * a * c;
 
+    if (a==0){
+      System.out.println("это вырожденное уравнение");
     }
-else {
-      if (d==0){
-        n=1;
-      }
-      else {
-        n=0;
-      }
+    if (d > 0) {
+      n = 2;
+
+    } else if (d == 0) {
+      n = 1;
+    } else {
+      n = 0;
     }
+
   }
+
   //doing metod that returns the result
-  public int rootNumber(){
+  public int rootNumber() {
     return n;
   }
 }
